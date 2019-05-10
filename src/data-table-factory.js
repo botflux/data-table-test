0811d0fc7465d56ff780c51776e17090f0f67546
Victor Mendele <victor.mendele68@gmail.com>
2019-05-10 11:22:34 +0200

    :pencil:docs: comments

--- src/data-table-factory.js
+++ src/data-table-factory.js
@@ -1,8 +1,18 @@
 import viewModelFactory from './view-model-factory'
 import viewFactory from './view-factory'
 
-const dataTableFactory = (getData, wrapper, { requestAfter = 250 } = {}) => {
+/**
+ * Create a new data table
+ * 
+ * @param {Function} getData 
+ * @param {HTMLElement} wrapper 
+ * @param {{}} param2 
+ * @param {EventTarget} errorHandler 
+ */
+const dataTableFactory = (getData, wrapper, { requestAfter = 250 } = {}, errorHandler) => {
     const eventTarget = new EventTarget ()
+
+    if (!errorHandler) errorHandler = new EventTarget ()
 
     viewModelFactory ({
         getData,
